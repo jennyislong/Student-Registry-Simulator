bbd9c96912ca06bbaf335ae152b4f4d905f68920
Jenny Long <56453932+jennyislong@users.noreply.github.com>
2021-01-02 14:08:08 -0500

    Create UnknownCourseException.java

---
+++ UnknownCourseException.java
@@ -0,0 +1,14 @@
+/**
+ * Jenny Long
+ */
+
+
+public class UnknownCourseException extends RuntimeException
+
+{
+	public UnknownCourseException() {}
+	public UnknownCourseException (String message)
+	{
+		super(message);
+	}
+}
