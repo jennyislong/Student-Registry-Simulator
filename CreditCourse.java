f173327e7c5b126798035815ad5ab40906977531
Jenny Long <56453932+jennyislong@users.noreply.github.com>
2021-01-02 14:06:23 -0500

    Create CreditCourse.java

---
+++ CreditCourse.java
@@ -0,0 +1,37 @@
+/**
+ * Jenny Long
+ */
+
+
+
+public class CreditCourse extends Course 
+{
+	private String  semester;
+	public  double  grade;
+	public  boolean active;
+	
+	public CreditCourse(String name, String code, String descr, String fmt,String semester, double grade)
+	{
+		// redundant 
+		super(name, code, descr, fmt);
+		this.semester = semester;
+		this.grade    = grade;
+		active = false;
+	}
+	
+	public void setActive()
+	{
+		active = true;
+	}
+	
+	public void setInactive()
+	{
+		active = false;
+	}
+	
+	public String displayGrade()
+	{
+		return getCode() + " " + getName() + " " + semester + " Grade " + convertNumericGrade(grade); 
+	}
+	
+}
